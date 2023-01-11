c4abd82674de2485cbe8f2f645a26b3c0f188f1d
YuLim2 <jyulim0120@gmail.com>
2023-01-11 13:52:55 +0900

    INIT: project setting

---
+++ src/main/java/com/onecredict/onecredictserver/OneCredictServerApplication.java
@@ -0,0 +1,13 @@
+package com.onecredict.onecredictserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCredictServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneCredictServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onecredict/onecredictserver/OneCredictServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onecredict.onecredictserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneCredictServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
