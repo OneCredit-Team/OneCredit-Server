132150a55cfcd76aefcb24fcc288e8720f215bf7
YuLim2 <jyulim0120@gmail.com>
2023-01-15 20:04:30 +0900

    ADD: Load All of place data and send csv file data to json

--- src/main/java/com/onecredict/onecredictserver/controller/PlaceController.java
+++ src/main/java/com/onecredict/onecredictserver/controller/PlaceController.java
@@ -1,32 +1,37 @@
 package com.onecredict.onecredictserver.controller;
 
 import com.onecredict.onecredictserver.controller.rq.SavePlaceRq;
-import com.onecredict.onecredictserver.controller.rs.FindPlaceByIdRs;
+import com.onecredict.onecredictserver.controller.rs.FindAllPlaceRs;
+import com.onecredict.onecredictserver.controller.rs.FindPlaceByAddressRs;
 import com.onecredict.onecredictserver.service.PlaceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.List;
 
+import static com.onecredict.onecredictserver.repository.CsvRepository.ConvertCsvtoJson;
+@CrossOrigin(origins="*", allowedHeaders = "*")
 @RequiredArgsConstructor
 @RestController
 public class PlaceController {
 
     private final PlaceService placeService;
 
-    @PostMapping("/api/place")
-    public void savePlace(@Validated @RequestBody SavePlaceRq rq){
+    @GetMapping("/api/place/all")
+    public List<FindAllPlaceRs> findAllPlace(){
+        return placeService.findAllPlace();
+    }
 
+    @PostMapping("/api/place/add")
+    public void savePlace(@Validated @RequestBody SavePlaceRq rq){
         placeService.savePlace(rq);
     }
 
-    @GetMapping("/api/place/{place-id}")
-    public FindPlaceByIdRs findPlaceById(@PathVariable("place-id") Long placeId){
-        return placeService.findPlaceById(placeId);
+    @GetMapping("/api/place/{address}")
+    public FindPlaceByAddressRs findPlaceByAddress(@PathVariable("address") String address){
+        return placeService.findPlaceByAddress(address);
     }
-
-
-
+    @GetMapping("/pyData")
+    public String showJson() throws Exception {return ConvertCsvtoJson();}
 }
